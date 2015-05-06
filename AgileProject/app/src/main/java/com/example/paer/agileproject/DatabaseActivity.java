b4020f691aae82e1d023c8480f8bed6ec23a0131
Marc Jamot <marcjamot@gmail.com>
2015-05-06 23:55:55 +0200

    Add navigation drawer

---
+++ AgileProject/app/src/main/java/com/example/paer/agileproject/DatabaseActivity.java
@@ -0,0 +1,67 @@
+package com.example.paer.agileproject;
+
+import android.database.Cursor;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import java.sql.SQLException;
+
+import database.DataBaseHandler;
+
+
+public class DatabaseActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        Cursor cur = null;
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.fragment_pivotal_tracker);
+        DataBaseHandler dbh = new DataBaseHandler(this);
+        try {
+            dbh.open(this);
+            dbh.insertUser(dbh,"Mazen","Rachid");
+            dbh.insertNote(dbh, 1, "The first note");
+            dbh.insertNote(dbh, 1, "MetaUser Notesssssssssssssssssssssss");
+            cur = dbh.selectUserNotes(1);
+            dbh.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+
+
+        if (cur.moveToFirst()) {
+            do {
+                Toast.makeText(getBaseContext(),cur.getInt(0)+"  "+cur.getString(1),Toast.LENGTH_LONG).show();
+            } while (cur.moveToNext());
+        }
+
+
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
--- AgileProject/app/src/main/java/com/example/paer/agileproject/MainActivity.java
+++ AgileProject/app/src/main/java/com/example/paer/agileproject/MainActivity.java
@@ -1,67 +1,85 @@
 package com.example.paer.agileproject;
 
-import android.database.Cursor;
-import android.support.v7.app.ActionBarActivity;
+import android.content.res.Configuration;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
-import android.widget.Toast;
+import android.widget.ListView;
 
-import java.sql.SQLException;
-
-import database.DataBaseHandler;
-
-
+/**
+ * @author Marc
+ * @version 1.0
+ */
 public class MainActivity extends ActionBarActivity {
+    private FragmentNavigationDrawer dlDrawer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        Cursor cur = null;
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_pivotal_tracker);
-        DataBaseHandler dbh = new DataBaseHandler(this);
-        try {
-            dbh.open(this);
-            dbh.insertUser(dbh,"Mazen","Rachid");
-            dbh.insertNote(dbh, 1, "The first note");
-            dbh.insertNote(dbh, 1, "MetaUser Notesssssssssssssssssssssss");
-            cur = dbh.selectUserNotes(1);
-            dbh.close();
-        } catch (SQLException e) {
-            e.printStackTrace();
+        setContentView(R.layout.activity_main);
+
+        // Set a Toolbar to replace the ActionBar.
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        // Find our drawer view
+        dlDrawer = (FragmentNavigationDrawer) findViewById(R.id.drawer_layout);
+        // Setup drawer view
+        dlDrawer.setupDrawerConfiguration((ListView) findViewById(R.id.lvDrawer), toolbar,
+                R.layout.menu_item, R.id.flContent);
+
+        // TODO: Here you add new navigation items
+        dlDrawer.addNavItem("First", "First Fragment", PivotalTrackerFragment.class);
+
+        // Select default
+        if (savedInstanceState == null) {
+            dlDrawer.selectDrawerItem(0);
         }
-
-
-
-        if (cur.moveToFirst()) {
-            do {
-                Toast.makeText(getBaseContext(),cur.getInt(0)+"  "+cur.getString(1),Toast.LENGTH_LONG).show();
-            } while (cur.moveToNext());
-        }
-
-
     }
 
+    @Override
+    public boolean onPrepareOptionsMenu(Menu menu) {
+        // If the nav drawer is open, hide action items related to the content
+        if (dlDrawer.isDrawerOpen()) {
+            // Uncomment to hide menu items
+            // menu.findItem(R.id.mi_test).setVisible(false);
+        }
+        return super.onPrepareOptionsMenu(menu);
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
+        MenuInflater inflater = getMenuInflater();
+        // Uncomment to inflate menu items to Action Bar
+        // inflater.inflate(R.menu.main, menu);
+        return super.onCreateOptionsMenu(menu);
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
+        // The action bar home/up action should open or close the drawer.
+        // ActionBarDrawerToggle will take care of this.
+        if (dlDrawer.getDrawerToggle().onOptionsItemSelected(item)) {
             return true;
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        // Sync the toggle state after onRestoreInstanceState has occurred.
+        dlDrawer.getDrawerToggle().syncState();
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        // Pass any configuration change to the drawer toggles
+        dlDrawer.getDrawerToggle().onConfigurationChanged(newConfig);
     }
 }
