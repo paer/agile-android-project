ae5bdb3cfc2199e3d62f60b543e9c5727518c914
Alex Tomescu <alex.dan.tomescu@gmail.com>
2015-05-28 11:45:10 +0200

    fixed project info page

--- AgileProject/app/src/main/java/com/example/paer/agileproject/fragments/ProjectInfoFragment.java
+++ AgileProject/app/src/main/java/com/example/paer/agileproject/fragments/ProjectInfoFragment.java
@@ -68,24 +68,22 @@ public class ProjectInfoFragment extends Fragment {
 
         projectNameText.setText(this.project);
 
-        if (branchListView.getChildCount() == 0) {
-            new GithubInfoAsyncTask(ProjectInfoFragment.this.getActivity()) {
-                @Override
-                protected void onPostExecute(GithubProjectInfoBundle githubProjectInfoBundle) {
-                    super.onPostExecute(githubProjectInfoBundle);
-                    SimpleDateFormat df = new SimpleDateFormat("MM/dd/yyyy HH:mm:ss");
-                    lastCommitMessage.setText(githubProjectInfoBundle.commits.get(0).getCommit().getMessage());
-                    lastCommitName.setText(githubProjectInfoBundle.commits.get(0).getCommit().getAuthor().getName());
-                    lastCommitTime.setText(df.format(githubProjectInfoBundle.commits.get(0).getCommit().getAuthor().getDate()));
-                    numberOfIssues.append(Integer.toString(githubProjectInfoBundle.issues.size()));
-                    for (String branch : githubProjectInfoBundle.branches) {
-                        TextView branchText = new TextView(getActivity());
-                        branchText.setText(branch);
-                        branchListView.addView(branchText);
-                    }
+        new GithubInfoAsyncTask(ProjectInfoFragment.this.getActivity()) {
+            @Override
+            protected void onPostExecute(GithubProjectInfoBundle githubProjectInfoBundle) {
+                super.onPostExecute(githubProjectInfoBundle);
+                SimpleDateFormat df = new SimpleDateFormat("MM/dd/yyyy HH:mm:ss");
+                lastCommitMessage.setText(githubProjectInfoBundle.commits.get(0).getCommit().getMessage());
+                lastCommitName.setText(githubProjectInfoBundle.commits.get(0).getCommit().getAuthor().getName());
+                lastCommitTime.setText(df.format(githubProjectInfoBundle.commits.get(0).getCommit().getAuthor().getDate()));
+                numberOfIssues.append(Integer.toString(githubProjectInfoBundle.issues.size()));
+                for (String branch : githubProjectInfoBundle.branches) {
+                    TextView branchText = new TextView(getActivity());
+                    branchText.setText(branch);
+                    branchListView.addView(branchText);
                 }
-            }.execute(new Pair(client, new GithubProject(project, owner)));
-        }
+            }
+        }.execute(new Pair(client, new GithubProject(project, owner)));
 
         return view;
     }
